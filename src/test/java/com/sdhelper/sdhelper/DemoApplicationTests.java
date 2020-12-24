cc7471ba42bf103784bcc75b679deb373c0cc5d2
Eduardo Marwell <eemarwell@gmail.com>
2020-12-24 13:59:38 -0400

    SpringBoot Configuration para testes.

--- src/test/java/com/sdhelper/sdhelper/DemoApplicationTests.java
+++ src/test/java/com/sdhelper/sdhelper/DemoApplicationTests.java
@@ -3,11 +3,13 @@ package com.sdhelper.sdhelper;
 import com.sdhelper.sdhelper.app.Main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootTest
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplicationTests {
 
 	public static void main(String[] args) {
