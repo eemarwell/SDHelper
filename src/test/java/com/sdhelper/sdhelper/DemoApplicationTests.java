136d7daa0698076983a36b9fd4b7a8431f71905e
Eduardo Marwell <eemarwell@gmail.com>
2020-12-24 13:03:03 -0400

    Teste de CI pro GitHub.

--- src/test/java/com/sdhelper/sdhelper/DemoApplicationTests.java
+++ src/test/java/com/sdhelper/sdhelper/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.sdhelper.sdhelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.marwell.sdhelper.Main.class)
 class DemoApplicationTests {
 
 	@Test
